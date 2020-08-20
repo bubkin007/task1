421a1af1836d52df1803131ddcf431192b62fc7c
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-20 13:38:15 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+               long fact = 1;
+        for (int i = 2; i <= 10; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
