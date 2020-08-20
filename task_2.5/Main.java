e1a6217e5e883f2ec7a7ab8a320a8b16dff66cd9
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-20 13:49:37 +0300

    Create Main.java

---
+++ task_2.5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[]mama = new String[]{"Мама", "Мыла", "Раму"};
+        System.out.println(mama[0] + mama[1]+mama[2]);
+        System.out.println(mama[0] + mama[2]+mama[1]);
+        System.out.println(mama[1] + mama[0]+mama[2]);
+        System.out.println(mama[1] + mama[2]+mama[0]);
+        System.out.println(mama[2] + mama[1]+mama[0]);
+        System.out.println(mama[2] + mama[0]+mama[1]);
+
+    }
+}
