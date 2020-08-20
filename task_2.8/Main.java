74165ac23eedfcffa256d1e85571134895461d75
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-20 14:20:44 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,9 @@
+              public class Main {
+    public static void main(String[] args) {
+        var v = "8";
+        for(int i = 0 ;i<10;i++){
+            System.out.println(v);
+                v=v+"8";
+
+    }
+}
