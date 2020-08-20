15ef74ba8f42aa1eb73ca10f5d77c28d8d4e8a62
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-20 13:41:20 +0300

    Create Main.java

---
+++ task_2.4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+var v = 0;
+        for(int i = 1; i<=5;i++){
+        v=v+i;
+            System.out.println(v);
+        }
+
+    }
+}
