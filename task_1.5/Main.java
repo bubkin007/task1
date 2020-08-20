72de338a1bdfcd2a872bcbf5731493f366e64c3a
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-20 13:03:59 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class helloworld {
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int TC = input.nextInt();
+        int  TF  = (9 / 5) * TC + 32;
+        System.out.println(TF );
+    }
+}
