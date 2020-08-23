b5b5e73e34c47e6cec19d70922856a5c0af71f55
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-23 19:05:53 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-public class helloworld {
-
-    public static void main(String[] args)
-    {
-        Scanner input = new Scanner(System.in);
-        int TC = input.nextInt();
-        int  TF  = (9 / 5) * TC + 32;
+public class Main {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        float TC = input.nextInt();
+        float  TF  = (float)(1.8* TC + 32);
         System.out.println(TF );
     }
 }
