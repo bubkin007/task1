dc33a75651016c0aac3081ace0aa8af88c9b88b0
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-26 13:09:42 +0300

    Create Cat.java

---
+++ task_4.1/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
