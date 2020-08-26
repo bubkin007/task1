3e68477fc7e91edc805d315b4eead384b8156fba
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-26 13:03:01 +0300

    Create Dog.java

---
+++ task_3.3/Dog.java
@@ -0,0 +1,22 @@
+    public class Dog {
+    
+    private String name;
+    int age;
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
