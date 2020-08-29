e08d8f7f42e0a67ff3a0842ae34eedeb03aa28df
Aleksandr Babkin <bubkin-007@ya.ru>
2020-08-29 23:52:13 +0300

    Update Main.java

--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -1,18 +1,47 @@
-public class Main {
+
+public class Solution {
     public static void main(String[] args) {
         Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Dog dog = new Dog("D", 12, 5);
+        Cat cat = new Cat("T", 12, 5);
+        //System.out.println(jerryMouse.name);
+        //System.out.println(cat.name);
 
         //напишите тут ваш код
     }
-}
-class Mouse {
-    String name;
-    int height;
-    int tail;
 
-    public Mouse(String name, int height, int tail) {
-        this.name = name;
-        this.height = height;
-        this.tail = tail;
+    public static class Mouse {
+        String name;
+        int height;
+        int tail;
+
+        public Mouse(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Zhiv{   //класс предок
+        String name;
+        int height;
+        int tail;
+        public Zhiv(String name, int height, int tail) {
+            this.name=name;
+            this.height=height;
+            this.tail=tail;
+        }
+    }
+
+    // теперь создаем потомков
+    public static class Dog extends Zhiv {
+        public Dog(String name, int height, int tail) {
+            super(name, height, tail);
+        }
+    }
+    public static class Cat extends Zhiv {
+        public Cat(String name, int height, int tail) {
+            super(name, height, tail);
+        }
     }
 }
