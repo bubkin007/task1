4f4798f737d9c96b1872f94c08dd54b97ac35a7a
Aleksandr Babkin <bubkin-007@ya.ru>
2020-09-02 16:42:58 +0300

    Update Main.java

--- task_4.3/Main.java
+++ task_4.3/Main.java
@@ -1,5 +1,32 @@
-public class Main{
-    public static void main(String[] args) {
-      //Ваш код
+package com.main;
+
+abstract class Animal{
+    //abstract method
+    public abstract void sound();
+
+}
+class Horce extends Animal{
+
+    public void run(){ System.out.println("Игого, я поскакал(а)"); }
+
+    @Override
+    public void sound() {
+    }
+}
+class Pegasus extends Animal{
+
+    public void fly(){ System.out.println("Игого, я полетел(а)"); }
+
+    @Override
+    public void sound() {
+
+    }
+}
+public class Main {
+    public static void main(String[] args)  {
+     Horce Horce = new Horce();
+     Horce.run();
+     Pegasus Pegasus = new Pegasus();
+     Pegasus.fly();
     }
 }
