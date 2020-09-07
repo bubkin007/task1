b86bddc60b325d7c33dbad15aedd916c108a5df3
Aleksandr Babkin <bubkin-007@ya.ru>
2020-09-07 22:40:53 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -1,25 +1,27 @@
-/* 
-1. Ввести имя файла с консоли.
-2. Прочитать из него набор чисел.
-3. Вывести на консоль только четные, отсортированные по возрастанию.
-Пример ввода:
-5
-8
--2
-11
-3
--5
-2
-10
-Пример вывода:
--2
-2
-8
-10
-*/
+package com.main;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(new FileInputStream(reader.readLine()));
+        String str;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            int temp = scanner.nextInt();
+            if (temp % 2 == 0) {
+                arrayList.add(temp);
+            }
+        }
+        scanner.close();
+        reader.close();
+        Collections.sort(arrayList);
+        for (Integer arr : arrayList) {
+            System.out.println(arr);
+        }
     }
 }
